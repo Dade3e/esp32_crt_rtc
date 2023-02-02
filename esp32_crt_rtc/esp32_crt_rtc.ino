679c538a866fabe6945ce3a92bd30d746cbf2e16
Davide Barchetta <dbarchetta@gheppio.hq.mquadro.net>
2023-02-02 15:07:54 +0100

    Aggiunto il set di data e ora

--- esp32_crt_rtc/esp32_crt_rtc.ino
+++ esp32_crt_rtc/esp32_crt_rtc.ino
@@ -1,40 +1,3 @@
-/*
-
-Example for ESP_8_BIT color composite video generator library on ESP32.
-Connect GPIO25 to signal line, usually the center of composite video plug.
-
-GFX Hello World
-
-This demonstrates using the ESP_8_BIT_GFX class, which inherits from the
-Adafruit GFX base class to deliver an easy to use graphics API. Draws two
-rectangles that cycle around the border of the screen. The amount of corners
-cut off from these rectangle show the amount of overscan on a particular
-screen. In the middle of two rectangles are a bit of text drawn using
-Adafruit GFX print() API.
-
-Copyright (c) Roger Cheng
-
-MIT License
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-*/
-
 #include <ESP_8_BIT_GFX.h>
 
 #include <ESP32Time.h>
@@ -43,43 +6,321 @@ SOFTWARE.
 // Create an instance of the graphics library
 ESP_8_BIT_GFX videoOut(0 /* = PAL */, 8 /* = RGB332 color */);
 
+ESP32Time rtc(0);  // offset in seconds GMT+1
 
-ESP32Time rtc(3600);  // offset in seconds GMT+1
+const int threshold = 40;
+
+//stato: 0 = inattivo, solo rtc
+//stato: 1 = mostra ora
+//stato: 2 = mostra data
+//stato: 3 = imposta ora
+//stato: 4 = imposta data
+//stato: 5 = conferma data
+int stato = 0;
+
+int stato_tmp = stato;
+
 
 void setup() {
-  // Initial setup of graphics library
   videoOut.begin();
+  Serial.begin(115200);
 
-  rtc.setTime(22, 10, 17, 30, 1, 2022);  // 17th Jan 2021 15:24:30
+  rtc.setTime(0, 0, 13, 2, 2, 2023);  // s m h d m y
+
+  videoOut.setTextColor(0xFF);
 }
 
+
 void loop() {
-  // Wait for the next frame to minimize chance of visible tearing
+
+  if(stato == 0){
+    spegni();
+    if(touchRead(4) < threshold || touchRead(27) < threshold){
+      stato = 1;
+      stato_tmp = 1;
+    }
+    waitRelease();
+  }
+
+  if(stato == 1){
+
+    printTime();
+
+    if(touchRead(4) < threshold){
+      stato_tmp = 2;
+    }
+
+    if(touchRead(27) < threshold){
+      stato_tmp = 2;
+    }
+
+    if(touchRead(4) < threshold && touchRead(27) < threshold){
+      stato_tmp = 3;
+    }
+
+    if(touchRead(4) > threshold && touchRead(27) > threshold){
+      stato = stato_tmp;
+    }
+  }
+
+  if(stato == 2){
+    printDate();
+
+    if(touchRead(4) < threshold){
+      stato_tmp = 1;
+    }
+
+    if(touchRead(27) < threshold){
+      stato_tmp = 1;
+    }
+
+    if(touchRead(4) < threshold && touchRead(27) < threshold){
+      stato_tmp = 4;
+    }
+    if(touchRead(4) > threshold && touchRead(27) > threshold){
+      stato = stato_tmp;
+    }
+  }
+
+  if(stato == 3){
+    setTime();
+    stato = 1;
+    stato_tmp = 1;
+  }
+
+  if(stato == 4){
+    setDate();
+    stato = 2;
+    stato_tmp = 2;
+  }
+  delay(100);
+
+}
+
+void waitRelease(){
+  while (touchRead(4) < threshold || touchRead(27) < threshold){
+    delay(10);
+  }
+}
+
+void spegni(){
   videoOut.waitForFrame();
-
-  // Get the current time and calculate a scaling factor
-  unsigned long time = millis();
-  // Clear screen
   videoOut.fillScreen(0);
-  /*
-  // Draw one rectangle
-  videoOut.drawLine(movingX, 0,       255,     movingY, cycle);
-  videoOut.drawLine(255,     movingY, invertX, 239,     cycle);
-  videoOut.drawLine(invertX, 239,     0,       invertY, cycle);
-  videoOut.drawLine(0,       invertY, movingX, 0,       cycle);
+  videoOut.setCursor(40, 100);
+  videoOut.setTextSize(4);
+  videoOut.setTextWrap(false);
+  videoOut.print("Spento");
+}
 
-  // Draw a rectangle with inverted position and color
-  videoOut.drawLine(invertX, 0,       255,     invertY, invertC);
-  videoOut.drawLine(255,     invertY, movingX, 239,     invertC);
-  videoOut.drawLine(movingX, 239,     0,       movingY, invertC);
-  videoOut.drawLine(0,       movingY, invertX, 0,       invertC);
-*/
-  // Draw text in the middle of the screen
-  videoOut.setCursor(0, 80);
-  videoOut.setTextColor(0xFF);
-  videoOut.setTextSize(9);
+void setTime(){
+  int tmp_h = rtc.getHour(true);
+  int tmp_min = rtc.getMinute();
+
+  waitRelease();
+
+  while(touchRead(4) > threshold || touchRead(27) > threshold ){
+    videoOut.waitForFrame();
+    videoOut.fillScreen(0);
+    videoOut.setCursor(40, 80);
+    videoOut.setTextSize(4);
+    videoOut.setTextWrap(false);
+    videoOut.print("Set Time");
+
+    videoOut.setCursor(30, 130);
+    videoOut.setTextSize(7);
+    videoOut.setTextWrap(false);
+    videoOut.print("H:");
+    if(touchRead(27) < threshold){
+      tmp_h++;
+      if(tmp_h>23)
+        tmp_h = 0;
+    }
+
+    if(touchRead(4) < threshold){
+      tmp_h--;
+      if(tmp_h < 0)
+        tmp_h = 23;
+    }
+
+    videoOut.setCursor(140, 130);
+    videoOut.setTextSize(7);
+    videoOut.setTextWrap(false);
+    videoOut.print(tmp_h);
+
+    delay(200);
+  }
+
+  waitRelease();
+
+  while(touchRead(4) > threshold || touchRead(27) > threshold ){
+    videoOut.waitForFrame();
+    videoOut.fillScreen(0);
+    videoOut.setCursor(40, 80);
+    videoOut.setTextSize(4);
+    videoOut.setTextWrap(false);
+    videoOut.print("Set Time");
+
+    videoOut.setCursor(30, 130);
+    videoOut.setTextSize(7);
+    videoOut.setTextWrap(false);
+    videoOut.print("M:");
+    if(touchRead(27) < threshold){
+      tmp_min++;
+      if(tmp_min>59)
+        tmp_min = 0;
+    }
+
+    if(touchRead(4) < threshold){
+      tmp_min--;
+      if(tmp_min < 0)
+        tmp_min = 59;
+    }
+
+    videoOut.setCursor(140, 130);
+    videoOut.setTextSize(7);
+    videoOut.setTextWrap(false);
+    videoOut.print(tmp_min);
+
+    delay(200);
+  }
+
+  waitRelease();
+
+  rtc.setTime(0, tmp_min, tmp_h, rtc.getDay(), rtc.getMonth(), rtc.getYear());
+}
+
+void setDate(){
+  int tmp_y = rtc.getYear();
+  int tmp_m = rtc.getMonth();
+  int tmp_d = rtc.getDay();
+
+  waitRelease();
+
+  while(touchRead(4) > threshold || touchRead(27) > threshold ){
+    videoOut.waitForFrame();
+    videoOut.fillScreen(0);
+    videoOut.setCursor(40, 80);
+    videoOut.setTextSize(4);
+    videoOut.setTextWrap(false);
+    videoOut.print("Set Date");
+
+    videoOut.setCursor(30, 130);
+    videoOut.setTextSize(7);
+    videoOut.setTextWrap(false);
+    videoOut.print("D:");
+    if(touchRead(27) < threshold){
+      tmp_d++;
+      if(tmp_d>31)
+        tmp_d = 0;
+    }
+
+    if(touchRead(4) < threshold){
+      tmp_d--;
+      if(tmp_d < 0)
+        tmp_d = 31;
+    }
+
+    videoOut.setCursor(140, 130);
+    videoOut.setTextSize(7);
+    videoOut.setTextWrap(false);
+    videoOut.print(tmp_d);
+
+    delay(200);
+  }
+
+  waitRelease();
+
+  while(touchRead(4) > threshold || touchRead(27) > threshold ){
+    videoOut.waitForFrame();
+    videoOut.fillScreen(0);
+    videoOut.setCursor(40, 80);
+    videoOut.setTextSize(4);
+    videoOut.setTextWrap(false);
+    videoOut.print("Set Date");
+
+    videoOut.setCursor(30, 130);
+    videoOut.setTextSize(7);
+    videoOut.setTextWrap(false);
+    videoOut.print("M:");
+    if(touchRead(27) < threshold){
+      tmp_m++;
+      if(tmp_m>12)
+        tmp_m = 0;
+    }
+
+    if(touchRead(4) < threshold){
+      tmp_m--;
+      if(tmp_m < 0)
+        tmp_m = 12;
+    }
+
+    videoOut.setCursor(140, 130);
+    videoOut.setTextSize(7);
+    videoOut.setTextWrap(false);
+    videoOut.print(tmp_m);
+
+    delay(200);
+  }
+
+  waitRelease();
+
+  while(touchRead(4) > threshold || touchRead(27) > threshold ){
+    videoOut.waitForFrame();
+    videoOut.fillScreen(0);
+    videoOut.setCursor(40, 80);
+    videoOut.setTextSize(4);
+    videoOut.setTextWrap(false);
+    videoOut.print("Set Date");
+
+    videoOut.setCursor(20, 130);
+    videoOut.setTextSize(6);
+    videoOut.setTextWrap(false);
+    videoOut.print("Y:");
+    if(touchRead(27) < threshold){
+      tmp_y++;
+      if(tmp_y>2100)
+        tmp_y = rtc.getYear();
+    }
+
+    if(touchRead(4) < threshold){
+      tmp_y--;
+      if(tmp_y < 2022)
+        tmp_y = 2100;
+    }
+
+    videoOut.setCursor(110, 130);
+    videoOut.setTextSize(6);
+    videoOut.setTextWrap(false);
+    videoOut.print(tmp_y);
+
+    delay(200);
+  }
+
+  waitRelease();
+
+  rtc.setTime(rtc.getSecond(), rtc.getMinute(), rtc.getHour(true), tmp_d, tmp_m, tmp_y);
+
+}
+
+void printTime(){
+  videoOut.waitForFrame();
+  videoOut.fillScreen(0);
+  videoOut.setCursor(0, 100);
+  videoOut.setTextSize(8);
   videoOut.setTextWrap(false);
   videoOut.print(rtc.getTime("%H:%M"));
-  delay(1000);
-  
+}
+
+void printDate(){
+  videoOut.waitForFrame();
+  videoOut.fillScreen(0);
+  videoOut.setCursor(40, 80);
+  videoOut.setTextSize(4);
+  videoOut.setTextWrap(false);
+  videoOut.print(rtc.getTime("%A"));
+
+  videoOut.setCursor(10, 130);
+  videoOut.setTextSize(5);
+  videoOut.setTextWrap(false);
+  videoOut.print(rtc.getTime("%d/%m/%y"));
 }
